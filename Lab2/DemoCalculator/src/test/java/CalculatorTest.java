e73baa8bde2bbc991bfde4f5f8294cf07ac2397f
nhattan0114 <nhattan0114.fudn@gmail.com>
2026-09-21 13:59:18 +0700

    test(Multiply): Test parameterized cho phuong thuc multipy

--- Lab2/DemoCalculator/src/test/java/CalculatorTest.java
+++ Lab2/DemoCalculator/src/test/java/CalculatorTest.java
@@ -3,6 +3,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
 public class CalculatorTest {
     private final Calculator calculator = new Calculator();
     @Test
@@ -50,4 +53,16 @@ public class CalculatorTest {
         int result =calculator.multiply(a,b);
         assertEquals(expected, result, "5 x 3 phải bằng 15");
     }
+    @ParameterizedTest(name = "Test {index} => {0} * {1} = {2}")
+    @CsvFileSource(resources = "/data.csv", numLinesToSkip = 1)
+    @DisplayName("multiply: kiểm thử với nhiều bộ dữ liệu từ CSV")
+    void multiply_VariousInputs_ReturnsProduct(int a, int b, int expected) {
+        // Arrange: a, b, expected do JUnit inject từ CSV
+        // Act
+        int actual = calculator.multiply(a, b);
+        // Assert
+        assertEquals(expected, actual,
+                () -> a + " * " + b + " phải bằng " + expected);
+    }
+
 }
